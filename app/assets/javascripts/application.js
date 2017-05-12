428cc3f5a1271b722233dbbc718e78e4a515497d
Eric Prostko <ericprostko9@gmail.com>
2017-05-12 16:42:39 -0400

    Initial Commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,33 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+$( document ).ready(function() {
+    $('.link-button').hover(function(){
+      $(event.target).css("border-bottom", "4px solid #8bc34a")
+      }, function() {
+      $(event.target).css("border-bottom", "none")
+      }
+    );
+    // there is some kind of race condition between pause() and play(), 150 ms is needed before promise comes in from pause
+    setTimeout(function () {
+      $('#homepage_video')[0].play();
+    }, 150);
+
+
+
+
+});
